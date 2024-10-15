12470c8636dd271a3de5ec5b96c0b28f9e1d0858
Jemerson Maia <110849015+jemersonln5@users.noreply.github.com>
2024-10-15 00:27:51 -0300

    autogen keypair

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -13,54 +13,65 @@ provider "aws" {
   region = "sa-east-1"
 }
 
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = file("~/.ssh/id_ed25519.pub")
+  key_name   = "biopark-ec2-key"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "local_file" "deployer" {
+  filename        = "biopark-ec2-key.pem"
+  content         = tls_private_key.rsa.private_key_pem
+  file_permission = "0400"
 }
 
 resource "aws_default_vpc" "default" {
 }
 
 resource "aws_security_group" "web" {
-  vpc_id      = aws_default_vpc.default.id
+  vpc_id = aws_default_vpc.default.id
 
   ingress {
-    description      = "HTTP Access"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTP Access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description      = "HTTPS Access"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTPS Access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description      = "SSH Access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0af6e9042ea5a4e3e"
-  instance_type = "t2.micro"
-  key_name = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [ aws_security_group.web.id ]
+  ami                    = "ami-052efd3df9dad4825"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.web.id]
 
   tags = {
     Name = "Wordpress"
